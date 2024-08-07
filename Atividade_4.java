0cce983418dbee478dd40ff7efb0994701153eb4
Isa <isabellavassoa@gmail.com>
2024-08-07 16:34:19 -0300

    Aula_9

---
+++ Atividade_4.java
@@ -0,0 +1,10 @@
+package Aula_9;
+
+public class Atividade_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Carros.java
@@ -0,0 +1,52 @@
+package Aula_9;
+
+public class Carros {
+
+
+
+	tring nome;
+	String cor;
+	String placa;
+	String marca;
+
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public String getMarca() {
+		return marca;
+
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+
+	}
+
+}
+
+
+}
+
+}
